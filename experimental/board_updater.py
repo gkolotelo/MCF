17deb8e43964ae8bc4b7b5534ddf369b5444cf65
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2014-11-14 01:09:15 -0500

    Added option to customize folder name

---
+++ experimental/board_updater.py
@@ -0,0 +1,66 @@
+# Board updater
+
+import datetime
+import sys
+import paramiko
+
+# Boards:
+boards = ['water_sensors_4.media.mit.edu']
+          #'water_sensors_2.media.mit.edu',
+          #'water_sensors_3.media.mit.edu',
+          #'water_sensors_4.media.mit.edu',
+          #'water_sensors_5.media.mit.edu',
+          #'water_sensors_6.media.mit.edu']
+
+try:
+    filename = str(sys.argv[1])
+except:
+    print "No files to copy!"
+    sys.exit(0)
+if filename == "-h" or filename == "--help":
+    print "No help!"
+    sys.exit(0)
+
+print "Confirm boards to be updated (Information on file): "
+for i in boards:
+    print i
+print "Confirm files to be updated: "
+for i in sys.argv[1:]:
+    print i
+res = raw_input("If the information above is correct, press enter to continue...")
+if res != "":
+    sys.exit(0)
+
+username = "root"
+password = "plantos"
+# username = raw_input("Enter username: ")
+# password = raw_input("Enter password: ")
+foldername = "RPi_" + raw_input("Enter foldername or leave blank for default: RPi_")
+
+print "\nProceeding with updates...\n\n\n"
+
+for board in boards:
+    print "Now updating " + board
+    print "Connecting by SSH..."
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(board, username=username, password=password)
+    print "Connected"
+    now = datetime.datetime.now()
+    if foldername != "":
+        foldername = "RPi_" + str(now.month) + '_' + str(now.day) + '_' + str(now.year) +\
+                     '_' + str(now.hour) + '_' + str(now.minute) + '_' + str(now.second)
+    print "Creating backup of current files on new folder: " + foldername
+    _, stdout, stderr = client.exec_command('cp -r RPi/ ' + foldername)
+    if len(stderr.readlines()) != 0:
+        print stderr.readlines()
+        print "Error copying files!"
+        continue
+    print "Done!"
+    print "Uploading, and overwriting, new files:"
+    sftp = client.open_sftp()
+    for argument in sys.argv[1:]:
+        info = sftp.put(argument, '/root/RPi/' + argument.split('/')[-1])
+        print "     Transfered " + argument.split('/')[-1] + ", " + str(info.st_size) + "bytes"
+    client.close()
+    print "Finished updating board " + board
